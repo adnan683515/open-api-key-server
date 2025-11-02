003d665b8e94202c8007d004f9f29838f2f5182f
Adnan <golamfaruk680@gmail.com>
2025-11-02 06:11:00 +0600

    open-ai-api-key setup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const OpenAI = require("openai");
 
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,13 +23,39 @@ const openAi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
 app.post("/prompt", async (req, res) => {
     try {
-        const result = await openAi.responses.create({
-            model: "gpt-4o-mini",
-            input: req?.body?.prompt
-        });
-        const output = result.output[0].content[0].text;
 
-        res.json({ reply: output });
+        const { prompt, image } = req?.body
+        console.log(prompt, image)
+
+
+        if (prompt && image) {
+            //image 
+            const response = await openAi.responses.create({
+                model: "gpt-4o-mini",
+                input: [{
+                    "role": "user",
+                    "content": [
+                        { "type": "input_text", "text": prompt ? prompt : 'image_tar_topics ki' },
+                        {
+                            "type": "input_image",
+                            "image_url": image,
+                        },
+                    ],
+                }],
+            });
+
+            res.json({ reply: response?.output_text });
+        }
+        else {
+            const result = await openAi.responses.create({
+                model: "gpt-4o-mini",
+                input: req?.body?.prompt
+            });
+            const output = result.output[0].content[0].text;
+            res.json({ reply: output });
+        }
+
+
     } catch (error) {
         console.error("Error:", error);
         res.status(500).json({ error: error.message });
@@ -36,7 +63,6 @@ app.post("/prompt", async (req, res) => {
 });
 
 app.post('/image-prompt', async (req, res) => {
-
 
     const { imageUrl, prompt } = req?.body
     console.log(imageUrl)
